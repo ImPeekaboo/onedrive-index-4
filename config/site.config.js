acb4145579c5de4b54bd4e3bac62bd7345b37379
Ayam Penyet <88007539+ImPeekaboo@users.noreply.github.com>
2023-05-14 00:56:22 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/ImPeekaboo/onedrive-index-4" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. 🥱 Ayam Penyet.',
+    'Powered by <a href="https://github.com/ImPeekaboo/onedrive-index-4" target="_blank" rel="noopener noreferrer">onedrive-index</a>. 🥱 Ayam Penyet.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
